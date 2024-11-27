a3f1134432e03b6e4055459651ce7b4ce574bc86
Matt Insley <mins@fb.com>
2024-11-27 13:19:30 -0800

    stub 2nd problem (wont compile cuz test globals, todo)

---
+++ 02-CubeConundrum/CubeConundrum.h
@@ -0,0 +1,15 @@
+//
+// Created by mins on 11/27/2024.
+//
+
+#ifndef CUBECONUNDRUM_H
+#define CUBECONUNDRUM_H
+
+class CubeConundrum
+{
+public:
+    static int solve1(const char* input);
+    static int solve2(const char* input);
+};
+
+#endif //CUBECONUNDRUM_H
