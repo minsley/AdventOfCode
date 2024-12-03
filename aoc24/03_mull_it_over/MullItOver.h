6ca98d17cea263c315f351a5b9aca440e543118a
Matt Insley <insley.matthew@gmail.com>
2024-12-03 00:52:27 -0800

    day3 pt 1

---
+++ aoc24/03_mull_it_over/MullItOver.h
@@ -0,0 +1,18 @@
+//
+// Created by mins on 12/2/2024.
+//
+
+#ifndef MULLITOVER_H
+#define MULLITOVER_H
+
+#include <vector>
+
+namespace MullItOver {
+    std::vector<std::vector<std::tuple<int,int>>> parse1(const std::string &filename);
+    std::vector<std::vector<std::tuple<int,int>>> parse2(const std::string &filename);
+    int mul(int a, int b);
+    int solve1(std::vector<std::vector<std::tuple<int,int>>> &program);
+    int solve2(std::vector<std::vector<std::tuple<int,int>>> &program);
+}
+
+#endif //MULLITOVER_H
---
+++ aoc24/03_mull_it_over/MullItOverPart2.cpp
@@ -0,0 +1,22 @@
+//
+// Created by mins on 12/2/2024.
+//
+
+#include <vector>
+
+#include "MullItOver.h"
+
+using namespace MullItOver;
+
+/*
+
+*/
+
+std::vector<std::vector<std::tuple<int,int>>> MullItOver::parse2(const std::string &filename) {
+    std::vector<std::vector<std::tuple<int,int>>> result;
+    return result;
+}
+
+int MullItOver::solve2(std::vector<std::vector<std::tuple<int,int>>> &program) {
+    return -1;
+}
