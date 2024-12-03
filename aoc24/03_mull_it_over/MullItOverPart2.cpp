559b26832398d144adc7f73df1a623837223d69a
Matt Insley <mins@fb.com>
2024-12-03 14:29:07 -0800

    futzing

--- aoc24/03_mull_it_over/MullItOverPart2.cpp
+++ aoc24/03_mull_it_over/MullItOverPart2.cpp
@@ -4,7 +4,6 @@
 
 #include "MullItOver.h"
 
-#include <iostream>
 #include <string>
 #include <regex>
 #include <vector>
@@ -20,9 +19,8 @@ using namespace MullItOver;
 std::vector<std::tuple<int,int>> MullItOver::parse2(const std::string &filename) {
     std::vector<std::tuple<int,int>> result;
 
-    std::regex re(R"(don't\(\).*?do\(\)|mul\((\d+),(\d+)\))", std::regex_constants::_S_multiline);
-
     auto fileStr = TestHelper::readFileToString(filename);
+    const std::regex re(R"(don't\(\).*?do\(\)|mul\((\d+),(\d+)\))");
     for(auto i = std::sregex_iterator(fileStr.begin(), fileStr.end(), re);
         i != std::sregex_iterator();
         *i++)
@@ -30,9 +28,7 @@ std::vector<std::tuple<int,int>> MullItOver::parse2(const std::string &filename)
         const std::smatch &m = *i;
         if(!m[1].matched || !m[2].matched) continue;
 
-        std::string aStr = m[1];
-        std::string bStr = m[2];
-        result.emplace_back(std::stoi(aStr), std::stoi(bStr));
+        result.emplace_back(std::stoi(m[1]), std::stoi(m[2]));
     }
 
     return result;
