49359e7e32d8a0fcba7c56c84bea520ef116f31e
Matt Insley <insley.matthew@gmail.com>
2024-12-03 19:21:26 -0800

    lets try rust

--- aoc24/cpp/03_mull_it_over/MullItOverPart2.cpp
+++ aoc24/cpp/03_mull_it_over/MullItOverPart2.cpp
@@ -14,16 +14,15 @@ using namespace MullItOver;
 Match [don't()...do()] blocks and mul(num,num) blocks that are outside of them, capture the number pairs.
 */
 
-std::vector<std::tuple<int,int>> MullItOver::parse2(const std::string &inputStr) {
-    std::vector<std::tuple<int,int>> result;
+std::vector<std::tuple<int, int>> MullItOver::parse2(const std::string &inputStr) {
+    std::vector<std::tuple<int, int>> result;
 
     const std::regex re(R"(mul\((\d+),(\d+)\)|don't\(\)(?:.|\n)*?(?:do\(\)|$))");
-    for(auto i = std::sregex_iterator(inputStr.begin(), inputStr.end(), re);
-        i != std::sregex_iterator();
-        *i++)
-    {
+    for (auto i = std::sregex_iterator(inputStr.begin(), inputStr.end(), re);
+         i != std::sregex_iterator();
+         *i++) {
         const std::smatch &m = *i;
-        if(!m[1].matched || !m[2].matched) continue;
+        if (!m[1].matched || !m[2].matched) continue;
 
         result.emplace_back(std::stoi(m[1]), std::stoi(m[2]));
     }
